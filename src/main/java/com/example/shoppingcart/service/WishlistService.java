6f58bd0092a0f1227b2431274e65507eb10ba5df
Khushisetia <102514281+Khushisetia@users.noreply.github.com>
2024-11-18 16:52:20 +0530

    Update WishlistService.java

--- src/main/java/com/example/shoppingcart/service/WishlistService.java
+++ src/main/java/com/example/shoppingcart/service/WishlistService.java
@@ -17,24 +17,24 @@ public class WishlistService {
     private WishlistRepo wishlistRepo;
 
     @Autowired
-    private ProductRepo productRepo; // Inject the product repository
+    private ProductRepo productRepo; 
 
     public List<WishlistItem> viewWishlist(String userId) {
         Wishlist wishlist = (Wishlist) wishlistRepo.findByUserId(userId).orElse(null);
         if (wishlist != null) {
-            return new ArrayList<>(wishlist.getProduct()); // Return the list of WishlistItem
+            return new ArrayList<>(wishlist.getProduct()); 
         }
         return List.of(); // Return an empty list if no wishlist found
     }
 
-    // Add a product to the wishlist
+    
     // Add a product to the wishlist
     public String addProductToWishlist(String userId, WishlistItem wishlistItem) {
 
         if (wishlistItem == null || userId == null) {
             throw new IllegalArgumentException("UserId or WishlistItem cannot be null");
         }
-        // Set the userId in the wishlistItem
+       
         Wishlist wishlist = wishlistRepo.findByUserId(userId).orElse(new Wishlist());
 
         // If the wishlist does not exist, create a new one
@@ -47,9 +47,9 @@ public class WishlistService {
                 .anyMatch(item -> item.getProductId().equals(wishlistItem.getProductId()));
 
         if (!exists) {
-            wishlist.getProduct().add(wishlistItem); // Add WishlistItem to the wishlist
-            wishlist.setUserId(userId); // Set userId in the Wishlist only
-            wishlistRepo.save(wishlist); // Save the updated wishlist back to the database
+            wishlist.getProduct().add(wishlistItem); 
+            wishlist.setUserId(userId); 
+            wishlistRepo.save(wishlist); 
             return "Product added successfully in wishlist.";
         } else {
             return "Product is already in the wishlist.";
@@ -61,7 +61,7 @@ public class WishlistService {
     public void removeProductFromWishlist(String userId, String productId) {
         Wishlist wishlist = (Wishlist) wishlistRepo.findByUserId(userId).orElse(null);
         if (wishlist != null) {
-            wishlist.getProduct().removeIf(item -> item.getProductId().equals(productId)); // Remove the WishlistItem
+            wishlist.getProduct().removeIf(item -> item.getProductId().equals(productId)); 
             wishlistRepo.save(wishlist); // Save the updated wishlist
         }
     }
